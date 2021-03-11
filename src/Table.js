345d16bed4c83fedcfb9fead051c01ccc9caeb25
Julia <yuliya@yuliya.com>
2021-03-11 15:06:45 +0300

    Add Table API service Added delete API call

--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios'
+import TableService from './TableService'
 
 class Table extends React.Component {
 
@@ -9,17 +10,30 @@ class Table extends React.Component {
             rows: []
         };
         this.addNewRecord = this.addNewRecord.bind(this);
+        this.deleteRecord = this.deleteRecord.bind(this);
     }
 
     componentDidMount() {
-        axios.get('http://178.128.196.163:3000/api/records')
-            .then(response => this.setState({rows: response.data}))
+        TableService.getRecords().then(res => this.setState({rows: res.data}))
     }
 
     addNewRecord() {
         let currentRecord = this.state;
-        currentRecord.rows.push({_id: '6047365bae66af078a510r45', data: {name: 'Bruce', age: 49, email: 'king@king.com'}});
+        currentRecord.rows.push({
+            _id: '6047365bae66af078a510r45',
+            data: {name: 'Bruce', age: 49, email: 'king@king.com'}
+        });
         this.setState(currentRecord);
+    }
+
+    deleteRecord(id) {
+        TableService.deleteRecord(id)
+            .then(res => {
+                this.setState({rows: this.state.rows.filter(rows => rows._id !== id)});
+            })
+            .catch(reason => {
+                console.error(reason)
+            });
     }
 
     render() {
@@ -45,6 +59,9 @@ class Table extends React.Component {
                             <td>{item.data.name}</td>
                             <td>{item.data.age}</td>
                             <td>{item.data.email}</td>
+                            <td>
+                                <button onClick={() => this.deleteRecord(item._id)}>Удалить</button>
+                            </td>
                         </tr>
                     )}
                     </tbody>
---
+++ src/TableService.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+class TableService {
+
+    getRecords() {
+        return axios.get('http://178.128.196.163:3000/api/records');
+    };
+
+    addRecord(record) {
+
+    };
+
+    deleteRecord(id) {
+        return axios.delete('http://178.128.196.163:3000/api/records/' + id);
+    };
+
+    updateRecord(id, record) {
+
+    };
+}
+
+export default new TableService()
